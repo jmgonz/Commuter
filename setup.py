7614be783631db5b97c5d8bc1d63657241b809e6
jmgonz <johnmgonzalez03@gmail.com>
2023-02-21 22:47:55 -0500

    Added stub software library, see #24

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='Commuter',
+      version='1.0',
+      description='Commuter Stub Library',
+      author='John Gonzalez Weijie Wang',
+      author_email='jgonzalez@nyu.edu',
+      py_modules=['commuter'],
+     )
