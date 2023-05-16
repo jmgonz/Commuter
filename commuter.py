daa0486cf55a8f1ca3b4f88b05905b3e97ce3796
weijiekwang <weijiekwang@gmail.com>
2023-05-15 21:19:33 -0400

    Update commuter.py

--- commuter.py
+++ commuter.py
@@ -27,83 +27,64 @@ def info():
     print("Software library for the Commuter project.")
 
 
-def lcd(heartrate):
-	disp = LCD_1inch28.LCD_1inch28()
-	# Initialize library.
-	disp.Init()
-
-	# Create blank image for drawing.
-	image1 = Image.new("RGB", (disp.width, disp.height), "BLACK")
-	draw = ImageDraw.Draw(image1)
-
-	# Green if heartrate is below threshold, red otherwise
-	fillColor = (0,255,0) if heartrate<100 else (255,0,0)
-	logging.info("draw circle")
-	draw.arc((1,1,239,239),0, 360, fill = fillColor)
-	draw.arc((2,2,238,238),0, 360, fill = fillColor)
-	draw.arc((3,3,237,237),0, 360, fill = fillColor)
-
-	logging.info("draw text")
-	Font = ImageFont.truetype("../Font/Font02.ttf",32)
-	text= u"Heart Rate: "+str(heartrate)
-	draw.text((40, 95),text, fill = "WHITE",font=Font)
-
-	im_r=image1.rotate(180)
-	disp.ShowImage(im_r)
-	time.sleep(2)
-
 
 def turnOnLCD():
-	# Raspberry Pi pin configuration:
-	RST = 27
-	DC = 25
-	BL = 18
-	bus = 0
-	device = 0
-	logging.basicConfig(level=logging.DEBUG)
-	try:
-		# display with hardware SPI:
-		disp = LCD_1inch28.LCD_1inch28()
-    	# Initialize library.
-		disp.Init()
-    	# Clear display.
-		disp.clear()
+    # display with hardware SPI:
+    disp = LCD_1inch28.LCD_1inch28()
+    # Initialize library.
+    disp.Init()
+    # Create blank image for drawing.
+    image1 = Image.new("RGB", (disp.width, disp.height), "BLACK")
+    draw = ImageDraw.Draw(image1)
 
-    	# Create blank image for drawing.
-		image1 = Image.new("RGB", (disp.width, disp.height), "BLACK")
-		draw = ImageDraw.Draw(image1)
+    updateHR()
+    # Green if heartrate is below threshold, red otherwise
+    fillColor = (0,255,0) if heartrate<100 else (255,0,0)
+    logging.info("draw circle")
+    draw.arc((1,1,239,239),0, 360, fill = fillColor)
+    draw.arc((2,2,238,238),0, 360, fill = fillColor)
+    draw.arc((3,3,237,237),0, 360, fill = fillColor)
 
+    logging.info("draw text")
+    Font = ImageFont.truetype("../Font/Font02.ttf",32)
+    text= u"Heart Rate: "+str(heartrate)
+    draw.text((40, 95),text, fill = "WHITE",font=Font)
 
-		logging.info("draw circle")
-		draw.arc((1,1,239,239),0, 360, fill =(0,0,255))
-		draw.arc((2,2,238,238),0, 360, fill =(0,0,255))
-		draw.arc((3,3,237,237),0, 360, fill =(0,0,255))
-
-
-		logging.info("draw text")
-		Font1 = ImageFont.truetype("../Font/Font01.ttf",25)
-		Font2 = ImageFont.truetype("../Font/Font01.ttf",35)
-		Font3 = ImageFont.truetype("../Font/Font02.ttf",32)
-
-		text= u"Heart Rate: 99"
-		draw.text((40, 95),text, fill = "WHITE",font=Font3)
-		im_r=image1.rotate(180)
-		disp.ShowImage(im_r)
-		time.sleep(10)
-
-	except IOError as e:
-		logging.info(e)
-	except KeyboardInterrupt:
-		disp.module_exit()
-		logging.info("quit:")
-		exit()
+    im_r=image1.rotate(180)
+    disp.ShowImage(im_r)
+    time.sleep(1)
+    return
 
 
+#  Changes screen circle to red to give user feedback
+def flashLCDRed():
+    # display with hardware SPI:
+    disp = LCD_1inch28.LCD_1inch28()
+    # Initialize library.
+    disp.Init()
+    image1 = Image.new("RGB", (disp.width, disp.height), "BLACK")
+    draw = ImageDraw.Draw(image1)
+    logging.info("draw circle")
+    draw.arc((1,1,239,239),0, 360, fill =(255,0,0))
+    draw.arc((2,2,238,238),0, 360, fill =(255,0,0))
+    draw.arc((3,3,237,237),0, 360, fill =(255,0,0))
+    logging.info("draw text")
+    Font1 = ImageFont.truetype("../Font/Font01.ttf",25)
+    Font2 = ImageFont.truetype("../Font/Font01.ttf",35)
+    Font3 = ImageFont.truetype("../Font/Font02.ttf",32)
+    text= u"Heart Rate: "+str(heartrate)
+    draw.text((40, 95),text, fill = "WHITE",font=Font3)
+
+    im_r=image1.rotate(180)
+    disp.ShowImage(im_r)
+    return
+		
 def refreshLCD():
     while True:
         turnOnLCD()
 
 
+# Turns screen on or off when user presses screen button
 def screenToggle():
 	if (screenStatus):
 		disp=LCD_1inch28.LCD_1inch28()
